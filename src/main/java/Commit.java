5c70618d7e898dd076f4cb731a566d64407b7ea1
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-02-18 10:13:51 +0000

    o

---
+++ src/main/java/Commit.java
@@ -0,0 +1,11 @@
+public class Commit {
+
+
+    private String description;
+    private int unique_id;
+}
+
+public Commit(String description, int unique_id ) {
+    this.description = description;
+    this.unique_id = unique_id;
+}
